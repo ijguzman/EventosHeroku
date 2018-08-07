80b0a521e1213914dac52c3429815dca0530ddeb
Unknown <intiguzmanc@gmail.com>
2018-08-07 17:53:58 -0500

    añadir dist

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
+
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/EventosHeroku'));
+app.use(express.static(path.join(__dirname + '/dist/Cliente-Eventos/')));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/EventosHeroku/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Cliente-Eventos/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port=app.listen(process.env.PORT || 8080);
+app.set('port',port);
+const server=http.createServer(app);
+server.listen(port,() => console.log('Running'));
