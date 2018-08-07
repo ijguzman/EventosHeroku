534c5b5f2492de77b745c63cd67575d77d3f084c
Unknown <intiguzmanc@gmail.com>
2018-08-07 17:28:04 -0500

    cambiar nombre app

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/eventos-cliente'));
+app.use(express.static(__dirname + '/dist/EventosHeroku'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/eventos-cliente/index.html'));
+res.sendFile(path.join(__dirname+'/dist/EventosHeroku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
